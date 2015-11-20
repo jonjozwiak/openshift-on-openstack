284afe455e7384fdf33a2a15da822ed614080fc2
jonjozwiak <jjozwiak@redhat.com>
2015-11-19 21:01:08 -0600

    Adding load balancer files

---
+++ fragments/lb-boot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+set -x
+set -o pipefail
+
+function notify_success() {
+    $WC_NOTIFY --data-binary  "{\"status\": \"SUCCESS\", \"reason\": \"$1\", \"data\": \"$1\"}"
+    exit 0
+}
+
+function notify_failure() {
+    $WC_NOTIFY --data-binary "{\"status\": \"FAILURE\", \"reason\": \"$1\", \"data\": \"$1\"}"
+    exit 1
+}
+
+# Set the DNS to the one provided
+sed -i 's/search openstacklocal/&\nnameserver $DNS_IP/' /etc/resolv.conf
+sed -i -e 's/^PEERDNS.*/PEERDNS="no"/' /etc/sysconfig/network-scripts/ifcfg-eth0
+
+# master and nodes
+retry yum install -y deltarpm
+retry yum -y update
+
+notify_success "Load balancer node has been prepared for running ansible."
