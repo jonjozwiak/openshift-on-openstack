1aa3d3bf23fea5c9d99346cd0faefa6418c918b2
jonjozwiak <jjozwiak@redhat.com>
2015-11-19 21:30:12 -0600

    Adding retry to installing os-collect-config agent

--- collect-config-setup/fragments/install_config_agent_yum.sh
+++ collect-config-setup/fragments/install_config_agent_yum.sh
@@ -4,9 +4,9 @@ set -eux
 # Setup RDO repo if not RHEL
 if [[ $(cat /etc/redhat-release | grep "Red Hat Enterprise" | wc -l) -gt 0 ]] 
 then
-  yum -y install os-collect-config python-zaqarclient os-refresh-config os-apply-config
+  retry yum -y install os-collect-config python-zaqarclient os-refresh-config os-apply-config
 else
-  yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-  yum -y install https://repos.fedorapeople.org/repos/openstack/openstack-kilo/rdo-release-kilo-1.noarch.rpm
+  retry yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+  retry yum -y install https://repos.fedorapeople.org/repos/openstack/openstack-kilo/rdo-release-kilo-1.noarch.rpm
   #yum-config-manager --disable 'epel*'
 fi
